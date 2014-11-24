173a2b2896847ad0e88d6aff09273da0912e7247
Florian Briand <florian.briand@bepatient.com>
2014-11-24 14:48:15 +0100

    Change conflict inducing script

--- induce_conflicts.py
+++ induce_conflicts.py
@@ -1,9 +1,13 @@
+# -*- coding: utf8 -*-
+
 from path import path
 
-for f in path('.').walkfiles('*.md'):
-    if f.fnmatch('README.md'):
+for f in path('.').walkfiles(u'*.md'):
+    if f.fnmatch(u'README.md'):
         continue
+    print u'Edition du fichier {0} ...'.format(f.name),
     with f.in_place() as (reader, writer):
         for number, line in enumerate(reader, 1):
-            writer.write(u'{0:3}: '.format(number))
+            writer.write(u'> ')
             writer.write(line)
+    print u'Terminé !'
